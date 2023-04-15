57a5d763ee4831d38c98015a599e07112b003d73
scttmars <55801303+scttmars@users.noreply.github.com>
2023-04-15 16:13:42 -0400

    removed int ops

--- src/main/java/com/minimals/newOps/IntOps/IntRoundValue/IntExample.java
+++
@@ -1,9 +0,0 @@
-package com.minimals.newOps.IntOps.IntRoundValue;
-
-public class IntExample {
-    public static void main(java.lang.String[] args) throws java.lang.Exception {
-        java.lang.System.out.println("Hello");
-        byte[] salt = {80,45,56};
-        javax.crypto.spec.PBEKeySpec("very_secure".toCharArray(), salt, Math.round(50));
-      }
-}
