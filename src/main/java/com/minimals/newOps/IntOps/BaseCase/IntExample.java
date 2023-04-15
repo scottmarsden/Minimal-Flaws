86d58eef87ed9dbe8ac08e73abec0b73dfacab16
scttmars <55801303+scttmars@users.noreply.github.com>
2023-04-15 15:58:45 -0400

    added base case for int

---
+++ src/main/java/com/minimals/newOps/IntOps/BaseCase/IntExample.java
@@ -0,0 +1,11 @@
+package com.minimals.newOps.IntOps.BaseCase;
+
+public class IntExample {
+    
+        public static void main(java.lang.String[] args) throws java.lang.Exception {
+            java.lang.System.out.println("Hello");
+            byte[] salt = {80,45,56};
+            javax.crypto.spec.PBEKeySpec("very_secure".toCharArray(), salt, 50);
+          }
+    
+}
---
+++ src/main/java/com/minimals/newOps/Interproc/Interproc4/CipherPack.java
@@ -0,0 +1,28 @@
+package com.minimals.newOps.Interproc.Interproc4;
+
+public class CipherPack {
+    private String propertyName = "AES/GCM/NoPadding";
+    public CipherPack A0(){
+        propertyName = "AES/GCM/NoPadding";
+        return this;
+    }
+    public CipherPack A1(){
+        propertyName = "AES/GCM/NoPadding";
+        return this;
+    }
+    public CipherPack A2(){
+        propertyName = "AES/GCM/NoPadding";
+        return this;
+    }
+    public CipherPack A3(){
+        propertyName = "AES/GCM/NoPadding";
+        return this;
+    }
+    public CipherPack B(){
+        propertyName = "AES";
+        return this;
+    }
+    public String getpropertyName(){
+        return propertyName;
+    }
+}
